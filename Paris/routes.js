2cd504b5ae6e392cabd2111868cd76253a50b2c8
mcleeder <68357049+mcleeder@users.noreply.github.com>
2021-03-15 10:55:12 -0700

    static file test

--- Paris/routes.js
+++ Paris/routes.js
@@ -1,4 +1,9 @@
+const express = require('express');
 const router = require('express').Router();
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'static')));
 
 router.get('/', (req, res) => {
     res.send("Paris!");
@@ -7,5 +12,10 @@ router.get('/', (req, res) => {
 router.get('/about', (req, res) => {
     res.send("About Paris!");
 });
+
+router.get('/puck', (req, res) => {
+    res.sendFile(path.join(__dirname, '/static/images/puck.jpg'));
+});
+
 
 module.exports = router;
